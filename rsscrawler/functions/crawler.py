d083b305ce9e432e318d99a2c90f0d3046a06baf
hasaku63 <takuyahashimoto1988@gmail.com>
2020-07-27 22:27:12 +0900

    add feed

--- rsscrawler/functions/crawler.py
+++ rsscrawler/functions/crawler.py
@@ -20,7 +20,8 @@ feed_urls = {
     'Connpass/株式会社Serverless Operations': 'https://slsops.connpass.com/ja.atom',
     'Connpass/PyData.Tokyo': 'https://pydatatokyo.connpass.com/ja.atom',
     'Connpass/Forkwell': 'https://forkwell.connpass.com/ja.atom',
-    'Connpass/TDDBC': 'https://tddbc.connpass.com/ja.atom'
+    'Connpass/TDDBC': 'https://tddbc.connpass.com/ja.atom',
+    'Connpass/Kubernetes Meetup Novice': 'https://k8s-novice-jp.connpass.com/ja.atom'
 }
 
 
